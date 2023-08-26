9d7e165b14ba6582571dcc808aadacab761c3d53
KITTIPOB PANGTRAKOON <63070010@kmitl.ac.th>
2023-08-26 21:28:36 +0700

    UPDATE

---
+++ model1.py
@@ -0,0 +1,78 @@
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Input, Lambda, Dense, Flatten
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img
+from tensorflow.keras.models import Sequential
+import numpy as np
+from glob import glob
+import matplotlib.pyplot as plt
+
+IMAGE_SIZE = [224, 224]
+
+TrainFolder = "C:/Modelandimage/Train"
+ValidateFolder = "C:\Modelandimage\Validate"
+
+myResnet = ResNet50(input_shape=IMAGE_SIZE +
+                    [3], weights='imagenet', include_top=False)
+
+print(myResnet.summary())
+
+for layer in myResnet.layers:
+    layer.trainable = False
+
+# classes
+Classes = glob('C:/Modelandimage/Train/*')
+print(Classes)
+
+classesNum = len(Classes)
+print(classesNum)
+
+PlusFlattenLayer = Flatten()(myResnet.output)
+
+prediction = Dense(classesNum, activation='softmax')(PlusFlattenLayer)
+
+model = Model(inputs=myResnet.input, outputs=prediction)
+
+print(model.summary())
+
+model.compile(
+    loss='categorical_crossentropy',
+    optimizer='adam',
+    metrics=['accuracy']
+)
+
+
+train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,
+                                   )
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory(
+    TrainFolder, target_size=(224, 224), batch_size=32, class_mode='categorical')
+test_set = test_datagen.flow_from_directory(ValidateFolder, target_size=(
+    224, 224), batch_size=32, class_mode='categorical')
+
+result = model.fit(training_set,
+                   validation_data=test_set,
+                   epochs=50,
+                   steps_per_epoch=len(training_set),
+                   validation_steps=len(test_set))
+
+# plot the accuracy
+plt.plot(result.history['accuracy'], label='train_acc')
+plt.plot(result.history['val_accuracy'], label='val_acc')
+plt.legend()
+plt.show()
+
+# plot the loss
+plt.plot(result.history['loss'], label='train_loss')
+plt.plot(result.history['val_loss'], label='val_loss')
+plt.legend()
+plt.show()
+
+# save the model
+model.save('C:/Modelandimage/myCarsModel.h5')
---
+++ model2.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+from tensorflow.keras.models import Model
+import cv2
+
+from keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import numpy as np
+
+categories = ['2017 Hatchback', '2018 Yaris ATIV',
+              '2019 Yaris ATIV', '2019 Yaris Sedan', '2020 Hatchback']
+
+# load model
+
+model = tf.keras.models.load_model('C:/Modelandimage/myCarsModel.h5')
+print(model.summary())
+
+
+def preprareImage(PathForImage):
+    image = load_img(PathForImage, target_size=(224, 224))
+    imgResult = img_to_array(image)
+    imgResult = np.expand_dims(imgResult, axis=0)
+    imgResult = imgResult / 255.
+    return imgResult
+
+
+testImage = "C:/Modelandimage/maxresdefault.jpg"
+
+imgForModel = preprareImage(testImage)
+resultArray = model.predict(imgForModel, verbose=1)
+answer = np.argmax(resultArray, axis=1)
+
+print(answer)
+
+index = answer[0]
+
+print("The predicted car is : " + categories[index])
+
+# show the image :
+
+img = cv2.imread(testImage)
+cv2.putText(img, categories[index], (10, 100),
+            cv2.FONT_HERSHEY_COMPLEX, 1.6, (255, 0, 0), 3, cv2.LINE_AA)
+cv2.imshow('image', img)
+cv2.waitKey(0)
